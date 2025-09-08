56f6250b4c8e90f8cabc82b2d2f090549e7d2571
Sanjaykumar-Sivakumar <sanjays23112004@gmail.com>
2025-09-08 23:36:33 +0530

    Add Vowels class to remove vowels from input string

--- LTMindtree Practice/Maxwell marks using Array/Penalty.java
+++ LTMindtree Practice/Maxwell marks using Array/Penalty.java
@@ -1,15 +1,25 @@
 import java.util.*;
-public class Penalty{
-    public static void main(String[] args)
+class Penalty{
+    static int PenaltySum(int arr[],int n)
     {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i] = sc.nextInt();
-        }
+        int sum =0;
         Arrays.sort(arr);
-        int penalty = 0;
+        sum = sum + Math.abs(arr[0] - arr[1]);
+        sum = sum + Math.abs(arr[n-1] - arr[n-2]);
+        for(int i =1;i<n-1;i++)
+        {
+            sum += Math.min(Math.abs(arr[i] - arr[i+1]),Math.abs(arr[i] - arr[i-1]));
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for(int i = 0;i<num;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(""+ PenaltySum(arr, num));
     }
 }
