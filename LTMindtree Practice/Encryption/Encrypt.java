b88ca1c7821914e336e77449bd12a524015dc5c1
Sanjaykumar-Sivakumar <sanjays23112004@gmail.com>
2025-09-08 15:11:53 +0530

    Add Encrypt class with encryption method and Penalty class for input handling

---
+++ LTMindtree Practice/Encryption/Encrypt.java
@@ -0,0 +1,26 @@
+public class Encrypt{
+    public static StringBuffer encrypt(String text, int s)
+    {
+        StringBuffer result =  new StringBuffer();
+        for(int i = 0;i<text.length();i++)
+        {
+            char c = text.charAt(i);
+            if(Character.isUpperCase(c))
+            {
+                char ch = (char) (((c - 'A'+s)% 26) +'A');
+                result.append(ch);
+            }
+            else if(Character.isLowerCase(c))
+            {
+                char ch = (char) (((c - 'a'+s)% 26) +'a');
+                result.append(ch);
+            }
+            else 
+            {
+                result.append(c);
+            }
+        
+        }
+        return result;
+    }
+}
---
+++ LTMindtree Practice/Maxwell marks using Array/Penalty.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Penalty{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int penalty = 0;
+    }
+}
