378c1b9f9d731fc46e41b75552d35577dd99113b
Sanjaykumar-Sivakumar <sanjays23112004@gmail.com>
2025-09-11 15:08:58 +0530

    Fix VC class to correctly calculate consonant count and remove vowels from output string

--- LTMindtree Practice/Vowels & consonats Problem/VC.java
+++ LTMindtree Practice/Vowels & consonats Problem/VC.java
@@ -1,10 +1,15 @@
+import java.util.*;
 class VC
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String WithoutV = s.replaceAll("[aeiouAEIOU]", " ");
+        String WithoutV = s.replaceAll("[aeiouAEIOU]", "");
         int vowelsCount = s.length() - WithoutV.length();
-        int consonantCount = s.replaceAll("[^a-zA-Z]"," ".length());
+        int consonantCount = s.replaceAll("[^a-zA-Z]"," ").length() - vowelsCount;
+        System.out.println("Vowel Count: "+vowelsCount);
+        System.out.println("Consonant Count: "+consonantCount);
+        System.out.println("String Without vowels: "+WithoutV);
+
     }
 }
