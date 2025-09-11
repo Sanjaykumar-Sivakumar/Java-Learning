3f859f77166c9057ea39ad030e633f58dd04b773
Sanjaykumar-Sivakumar <sanjays23112004@gmail.com>
2025-09-11 15:01:09 +0530

    Fix VC class to correctly count vowels and consonants in input string

--- LTMindtree Practice/Vowels & consonats Problem/VC.java
+++ LTMindtree Practice/Vowels & consonats Problem/VC.java
@@ -3,6 +3,8 @@ class VC
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String WithoutV = s.replaceAll("[aeiouAEIOU]", s)
+        String WithoutV = s.replaceAll("[aeiouAEIOU]", " ");
+        int vowelsCount = s.length() - WithoutV.length();
+        int consonantCount = s.replaceAll("[^a-zA-Z]"," ".length());
     }
 }
