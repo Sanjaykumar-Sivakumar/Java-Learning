8e7c951f7d830868878604ce0dca4d81ef80ebb1
Sanjaykumar-Sivakumar <sanjays23112004@gmail.com>
2025-09-19 16:25:56 +0530

    Refactor pattern generation methods for improved readability and consistency

--- JAVA Basic Examples/Pattern Based Problems/pattern.java
+++ JAVA Basic Examples/Pattern Based Problems/pattern.java
@@ -1,69 +1,57 @@
+
 import java.util.*;
+
 class pattern {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
         // Number Increasing Pattern
-         for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= i;j++)
-            {
-                System.out.print(j+" ");
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
         // Number Increasing Reverse Pattern
-        for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= r - i + 1;j++)
-            {
-                System.out.print(j+" ");
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i + 1; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
         // Number Repetiton Pattern
-         for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= i;j++)
-            {
-                System.out.print(i+" ");
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
         // Number Reptition Reverse Pattern
-        for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= r - i + 1;j++)
-            {
-                System.out.print(i+" ");
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i + 1; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
-         System.out.println("----------------------");
+        System.out.println("----------------------");
         // Square Pattern
-         for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= r;j++)
-            {
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
         // Hollow Square Pattern
-        for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= r;j++)
-            {
-                if(i == 1 || j == 1 || i == r || j == r)
-                {
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r; j++) {
+                if (i == 1 || j == 1 || i == r || j == r) {
                     System.out.print("* ");
-                }
-                else 
-                {
+                } else {
                     System.out.print("  ");
                 }
             }
@@ -71,16 +59,11 @@ class pattern {
         }
         System.out.println("----------------------");
         // Hollow without space in 1st line Square Pattern
-        for(int i = 0;i<=r;i++)
-        {
-            for(int j = 0;j<= r;j++)
-            {
-                if(i == 0 || j == 0 || i == r || j == r)
-                {
+        for (int i = 0; i <= r; i++) {
+            for (int j = 0; j <= r; j++) {
+                if (i == 0 || j == 0 || i == r || j == r) {
                     System.out.print("*");
-                }
-                else 
-                {
+                } else {
                     System.out.print(" ");
                 }
             }
@@ -88,20 +71,16 @@ class pattern {
         }
         System.out.println("----------------------");
         // Right Triangle Pattern
-         for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= i;j++)
-            {
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
         // Reverse Right Triangle Pattern
-        for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= r - i + 1;j++)
-            {
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -109,136 +88,132 @@ class pattern {
         System.out.println("----------------------");
         // K - Pattern
         // Upper Half
-         for(int i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<= r - i + 1;j++)
-            {
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         //  Lower Half
-        for(int i = 1;i<r;i++)
-        {
-            for(int j = 1;j<=i + 1;j++)
-            {
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j <= i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
-         //Zero one triangle pattern
-        for(int  i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<=i;j++)
-            {
-                if((i + j)%2==0)
-                {
-                    System.out.print(1+ " ");
+        //Zero one triangle pattern
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
                 }
-                else System.out.print(0 + " ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
-         //Number Changing Pyramid Pattern
-         int num = 1;
-        for(int  i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<=i;j++)
-            {
-                System.out.print(num+" ");
+        //Number Changing Pyramid Pattern
+        int num = 1;
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
                 num++;
             }
             System.out.println();
         }
-         System.out.println("----------------------");
-         //Number Changing Pyramid Pattern
-        for(int  i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<=i;j++)
-            {
-                System.out.print(num+" ");
+        System.out.println("----------------------");
+        //Number Changing Pyramid Pattern
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
                 num++;
             }
             System.out.println();
         }
-         System.out.println("----------------------");
-          //Left half Pyramid Pattern
-        for(int  i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<=r - i;j++)   
-            {
+        System.out.println("----------------------");
+        //Left half Pyramid Pattern
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i; j++) {
                 System.out.print("  ");
             }
-            for(int k = 1;k<=i;k++) 
-            {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
         //Left half reverse Pyramid Pattern
-        for(int  i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<i;j++)   // 1<1 no 1<2 true 
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j < i; j++) // 1<1 no 1<2 true 
             {
                 System.out.print("  ");
             }
-            for(int k = 1;k<=r - i+ 1;k++) // r - i + 1 "reverse" condition
+            for (int k = 1; k <= r - i + 1; k++) // r - i + 1 "reverse" condition
             {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
-        //Triangle Pyramid Pattern
-        for(int  i = 1;i<=r;i++)
-        {
-            for(int j = 1;j<=r - i;j++)
-            {
+        //Left aligned Triangle Pyramid Pattern 
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i; j++) {
                 System.out.print(" ");
             }
-            for(int k = 1;k<=i;k++)
-            {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println("----------------------");
-        //Upper half
-        for(int i = 1;i<=r;i++)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                if(i == 4 || j == 2 || j == 3)
-                {
-                    System.out.print("  ");
-                }
-                else
-                {
-                    System.out.print("* ");
-                }
-            }
-            System.out.println();
-        }
-        //Lower Half
-        for(int i =1 ;i<=r;i++)
-        {
-            for(int j = 2 ;j<=r - i + 1;j++)
-            {
-                if(i==5 || j == 2)
-                {
+        //Orginal triangle
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i; j++) {
                 System.out.print("  ");
-                }
-                else
-                {
-                    System.out.print("* ");
-                }
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("----------------------");
+        //Inverted Pyramid triangle
+        for (int i = r; i >= 1; i--) {
+            // spaces before stars
+            for (int j = 1; j <= r - i; j++) {
+                System.out.print("  ");
+            }
+            // stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
 
-
+        System.out.println("----------------------");
+        //Number Triangle Pattern
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= r - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("----------------------");
+        //Reverse Triangle Number  Pattern
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= r; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+        
     }
 }
