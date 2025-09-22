4e9f10f4b4456cca103f9c768573e1caaf0f692c
Sanjaykumar-Sivakumar <sanjays23112004@gmail.com>
2025-09-22 21:47:11 +0530

    Add Rhombus pattern generation method

--- JAVA Basic Examples/Pattern Based Problems/pattern.java
+++ JAVA Basic Examples/Pattern Based Problems/pattern.java
@@ -214,6 +214,19 @@ class pattern {
             }
             System.out.println();
         }
-        
+        System.out.println("----------------------");
+        //Rhombus Pattern
+        for(int i = 1;i<=r;i++)
+        {
+            for(int j = 1;j<=r - i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
